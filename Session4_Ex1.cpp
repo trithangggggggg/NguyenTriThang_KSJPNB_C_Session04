742beb67632d846a572082d9718ce9188776c375
trithangggggggg <trithang2006vn@gmail.com>
2024-11-22 17:45:36 +0700

    Add files via upload

---
+++ Session4_Ex1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	int songuyen; 
+	printf("Moi nhap 1 so nguyen: ");
+	scanf("%d",&songuyen);
+	if (songuyen>=0){
+		printf("Day la so duong "); 
+	}else{
+		printf("Day là so am");
+	}
+	return 0; 
+}
+ 
+
+
+
