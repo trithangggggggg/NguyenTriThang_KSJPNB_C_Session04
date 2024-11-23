23d1a16b1ce80b6e346ad285455fd361636064d3
trithangggggggg <trithang2006vn@gmail.com>
2024-11-23 22:26:56 +0700

    Add files via upload

---
+++ Session4_Ex5.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <stdio.h>
+
+int main() {
+    int num1;
+	int num2;
+	int num3;
+    printf("Moi nhap so thu 1 : ");
+    scanf("%d", &num1);
+    printf("Moi nhap so thu hai: ");
+    scanf("%d", &num2);
+    printf("Moi nhap so thu ba: ");
+    scanf("%d", &num3);
+    if ((num3 > num1 && num3 < num2) || (num3 > num2 && num3 < num1)) {
+        printf("So %d nam trong khoang giua %d và %d.\n", num3, num1, num2);
+    } else {
+        printf("So %d khong nam trong khoang giua %d và %d.\n", num3, num1, num2);
+    }
+    return 0;
+}
+
